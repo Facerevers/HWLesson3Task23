7a77e825cab68b8682ffd858762656c0006aa3d9
crazle <crazle@mail.ru>
2023-07-11 16:34:43 +0300

    Таблица кубов чисел от 1 до N

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+void ShowCubes(int number)
+{
+    for (int count1 = 1; count1 < number+1; count1++)
+    {
+        Console.Write($"{Math.Pow(count1,3)} ");
+    }
+}
+
+int GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInput("Введите число: ");
+Console.Write($"{number} -> ");
+ShowCubes(number);
